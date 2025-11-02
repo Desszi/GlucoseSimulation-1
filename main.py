e828138f1f6a8687ad9912fe3e1252de94e339fd
Mercedesz Porkolab <mercedesz.porkolab@sap.com>
2025-11-02 15:42:59 +0100

    PPO
    PPO

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from simulation_core import (
 # === Main Entry Point ===
 def main():
     # Setup Config 
-    config = SimulationConfig(model_type="TD3")
+    config = SimulationConfig(model_type="PPO")
     patient_params = config.get_patient_params()
     print(f"Patient {config.patient_name} | BW: {patient_params['bw']} kg")
 
